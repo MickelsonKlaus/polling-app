89a772468cca6fac480203d777eee726e5d07a2c
MickelsonKlaus <klausmickelson52@email.com>
2022-07-22 00:44:56 +0100

    fix: owner able to vote error

--- src/pages/voting.tsx
+++ src/pages/voting.tsx
@@ -13,6 +13,7 @@ interface Poll {
 }
 
 const Voting = () => {
+    let isCreatedByCurrentUser = Cookies.get('pollCreatedByCurrentUser') ? true : false
     let params = useParams();
     let [voted, setVoted] = useState<boolean>(false);
     let [closing, setClosing] = useState<boolean>(false);
@@ -81,7 +82,7 @@ const Voting = () => {
         <section className="max-w-lg mx-auto block">
             <Link to="/" className="text-xs block mb-5 font-medium text-white bg-[#1C538E] w-fit p-2 opacity-70 transition-transform hover:scale-105 duration-200 hover:opacity-100">Back to home</Link>
             {!loading && !err ? <>{poll.title && <h1 className="text-xl font-medium block w-full bg-transparent pb-3 sm:text-2xl lg:text-3xl mb-10 text-white border-b border-gray-400">{poll.title}</h1>}
-                {((Cookies.get('pollCreatedByCurrentUser') || poll.closed) || Cookies.get('voted')) ? poll.options.map((value: string, i: number) => {
+                {((isCreatedByCurrentUser || poll.closed) || Cookies.get('voted')) ? poll.options.map((value: string, i: number) => {
                     return <div key={i} className="block w-full text-white font-medium mb-3 rounded-sm text-sm  border border-[#1C538E] relative">
                         <span style={{
                             width: `${((poll.votes[i] / totalVotes) * 100).toFixed()}%`
@@ -96,10 +97,10 @@ const Voting = () => {
                             }} className={`absolute w-0 h-full top-0 left-0 bg-[#1C538E]`}></span>
                             <button disabled={voted} className="w-full p-2 h-full relative z-10 text-left flex justify-between items-center" type="button" onClick={() => handle(i)}><span>{value}</span>{voted && <span>{((poll.votes[i] / totalVotes) * 100).toFixed()}%</span>}</button></div>
                     })}
-                {poll.title && Cookies.get('pollCreatedByCurrentUser') && <button type="button" style={{
+                {poll.title && isCreatedByCurrentUser && <button type="button" style={{
                     opacity: closing ? "opacity-50" : "opacity-100"
                 }} disabled={closing} className="outline-none mt-10 w-fit block mx-auto text-sm bg-[#1C538E] text-white py-2 px-5 rounded-sm transition-transform hover:scale-105 duration-200" onClick={handleToggle}>{closing ? poll.closed ? "Opening poll" : "Closing poll" : closed ? "Poll closed, Re-open" : "Close poll"}</button>}
-                {(Cookies.get('pollCreatedByCurrentUser') || poll.closed) && <p className="text-white mt-5 text-xs font-medium">Votes: {totalVotes}</p>}
+                {(isCreatedByCurrentUser || poll.closed) && <p className="text-white mt-5 text-xs font-medium">Votes: {totalVotes}</p>}
                 {poll.closed && <p className="text-white mt-3 text-xs font-medium opacity-70">This poll has been closed. Here are the results</p>}</> : loading && !err ? <p className="text-white mt-5 font-medium text-center">Loading...</p> : <p className="text-white mt-5 font-medium text-center">Poll not found</p>}
         </section>
     )
